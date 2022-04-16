89b20789e204fe933c9ab1d3569279021f1c0ade
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-04-16 23:16:28 +0200

    Ex02 done
    Missed the last part of the exercise.

--- PPP_Chapter_20_Ex02.cpp
+++ PPP_Chapter_20_Ex02.cpp
@@ -47,6 +47,16 @@ std::vector<double>* get_from_jill()
     return v;
 }
 
+// Return a pointer to the element in [first,last) that has the highest value
+double* high(double* first, double* last)
+{
+    double h = -1;
+    double* high;
+    for(double* p = first; p!=last; ++p)
+        if (h<*p) { high = p; h = *p; }
+    return high;
+}
+
 int main()
 try {
     int count{0};
@@ -61,6 +71,16 @@ try {
     for(int i=0; i<(*v_Jill).size();++i)
         std::cout<<(*v_Jill)[i]<<' ';
     std::cout<<"}\n";
+
+    double* jack_high=high(jack_data, jack_data+count);
+    std::cout<<"Jack's max: "<<*jack_high<<'\n';
+
+    std::vector<double>& v= *v_Jill;
+    double* jill_high=high(&v[0], &v[0]+v.size());
+    std::cout<<"Jill's max: "<<*jill_high<<'\n';
+
+    delete[] jack_data;
+    delete v_Jill;
 }
 catch (std::exception& e) {
 	std::cerr << "exceptison: " << e.what() << std::endl;
--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 //	* Programming Principles and Practice Using C++, Second Edition, Bjarne Stroustrup *
 //	************************************************************************************
 //
-//  2. Get the Jack-and-Jill example from §20.1.2 to work. Use input from a
-//  couple of small files to test it.
+//  3. Look at the palindrome examples (§18.7); redo the Jack-and-Jill example
+//  from §20.1.2 using that variety of techniques.
 
 //#include "std_lib_facilities.h"
 #include <iostream>
@@ -16,51 +16,9 @@
 #include <list>
 #include <array>
 
-double* get_from_jack(int* count)
-{
-    std::string iname{"jack_data.txt"};
-    std::ifstream ist{iname.c_str()};
-    if(!ist) std::cerr<<"can't open input file "<<iname<<'\n';
-
-    std::vector<double> v;
-    double d{};
-    while(ist>>d) v.push_back(d);
-
-    *count= v.size();
-    double* data = new double[*count];
-    for(int i =0; i<v.size();++i)
-        data[i]=v[i];
-
-    return data;
-}
-
-std::vector<double>* get_from_jill()
-{
-    std::string iname{"jill_data.txt"};
-    std::ifstream ist{iname.c_str()};
-    if(!ist) std::cerr<<"can't open input file "<<iname<<'\n';
-
-    std::vector<double>* v = new std::vector<double>;
-    double d{};
-    while(ist>>d) (*v).push_back(d);
-
-    return v;
-}
-
 int main()
 try {
-    int count{0};
-    double* jack_data=get_from_jack(&count);
-    std::cout<<"Data from Jack: { ";
-    for(int i=0; i<count;++i)
-        std::cout<<jack_data[i]<<' ';
-    std::cout<<"}\n";
 
-    std::vector<double>* v_Jill=get_from_jill();
-    std::cout<<"Data from Jill: { ";
-    for(int i=0; i<(*v_Jill).size();++i)
-        std::cout<<(*v_Jill)[i]<<' ';
-    std::cout<<"}\n";
 }
 catch (std::exception& e) {
 	std::cerr << "exceptison: " << e.what() << std::endl;
