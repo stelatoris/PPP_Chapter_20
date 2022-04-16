95249bd573715b83b66e86715f2f33e69cae3080
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-04-17 00:06:59 +0200

    Ex03 WIP

--- main.cpp
+++ main.cpp
@@ -16,6 +16,73 @@
 #include <list>
 #include <array>
 
+
+double* get_from_jack(int* count)
+{
+    std::string iname{"jack_data.txt"};
+    std::ifstream ist{iname.c_str()};
+    if(!ist) std::cerr<<"can't open input file "<<iname<<'\n';
+
+    std::vector<double> v;
+    double d{};
+    while(ist>>d) v.push_back(d);
+
+    *count= v.size();
+    double* data = new double[*count];
+    for(int i =0; i<v.size();++i)
+        data[i]=v[i];
+
+    return data;
+}
+
+std::vector<double>* get_from_jill()
+{
+    std::string iname{"jill_data.txt"};
+    std::ifstream ist{iname.c_str()};
+    if(!ist) std::cerr<<"can't open input file "<<iname<<'\n';
+
+    std::vector<double>* v = new std::vector<double>;
+    double d{};
+    while(ist>>d) (*v).push_back(d);
+
+    return v;
+}
+
+// Return a pointer to the element in the array a[] that has the highest value
+double* high_array(double a[], int n)
+{
+	if(n==0) return 0;
+	double* highest = &a[0];
+	for(int i = 0; i<n;++i){
+		if (a[i]>*highest) highest=&a[i];
+	}
+	return highest;
+}
+
+// Return a pointer to the element in the vector v that has the highest value
+double* high_vector( std::vector<double>& v)
+{
+	if(v.size()==0) return 0;
+	double* highest = &v[0];
+	for(int i = 0; i<v.size();++i){
+		if (v[i]>*highest) highest=&v[i];
+	}
+	return highest;
+}
+
+// Return a pointer to the element in [first,last) that has the highest value
+double* high_pointer( double* first, double* last)
+{
+	if(first==last)return 0;
+
+	double h = -1;
+    double* high;
+    for(double* p = first; p!=last; ++p)
+        if (h<*p) { high = p; h = *p; }
+    return high;
+
+}
+
 int main()
 try {
 
